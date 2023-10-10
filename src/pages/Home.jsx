28be3a35f33900f2b6b827caf47ad0bb97cb36c6
smokeysde <wermelskirchenmaximilian@gmail.com>
2023-10-10 16:09:57 +0200

    doing stuff

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,33 +1,58 @@
-import React from "react";
-import {useState} from "react";
-import {useEffect} from "react";
+import React, { useState } from "react";
 import axios from "axios";
+import { TextField, Button } from "@mui/material";
+import Box from "@mui/material/Box";
 
 const Home = () => {
-    const [data, setData] = useState([]);
-    useEffect(() => {
-        const fetchData = async () => {
-            try{
-                const res = await axios.get('http://localhost:5000/user');
-                setData(res.data);
-            }catch (e) {
-                console.log(e);
-            }
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
 
+    const handleUsernameChange = (event) => {
+        setUsername(event.target.value);
+    };
+
+    const handlePasswordChange = (event) => {
+        setPassword(event.target.value);
+    };
+
+    const handleLogin = async () => {
+        try {
+            const res = await axios.get("http://18.185.110.14:5000/api/user");
+            const userData = res.data[0];
+
+            if (userData.uname === username && userData.pass === password) {
+                window.location.href = "/dashboard";
+            } else {
+                console.log("Invalid credentials");
+            }
+        } catch (error) {
+            console.error("Login failed", error);
         }
-        fetchData();});
+    };
+
     return (
         <div>
-            <h1>Home</h1>
-            <ul>
-                {data.map(item => (
-                    <li key={item.idUser}>
-                        <h3>{item.User_Firstname} {item.User_Lastname}</h3>
-                        <p>{item.User_Email}</p>
-                        <p>{item.User_Lastname}</p>
-                    </li>
-                ))}
-            </ul>
+            <h1 className=" flex font-bold text-2xl justify-center ">Login</h1>
+            <Box sx={{display: 'flex', flexDirection: 'column', justifyContent: 'center'}}>
+                <TextField
+                    id="outlined-basic-username"
+                    label="Username"
+                    variant="outlined"
+                    value={username}
+                    onChange={handleUsernameChange}
+                />
+                <TextField
+                    id="outlined-basic-password"
+                    label="Password"
+                    variant="outlined"
+                    value={password}
+                    type={"password"}
+                    onChange={handlePasswordChange}
+                />
+                <Button variant="contained" color="primary" onClick={handleLogin}>
+                    Login
+                </Button>
+            </Box>
         </div>
     );
 };
